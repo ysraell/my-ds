fa573ca61124438a286cfb40c2c7524fb7e21f58
Israel Oliveira <prof.israel@gmail.com>
2021-09-13 22:54:14 -0300

    WIP

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,7 +5,7 @@ CI_COMMIT_TAG=$2
 EXT_SETTINGS=$1
 
 function settings2env() {
-    echo `grep $1: $2 |cut -d ':' -f 2`
+    echo `grep $1: $2 |cut -d ':' -f 2 |xargs`
 }
 
 if [ -z $2 ] ;
@@ -24,16 +24,15 @@ fi
 
 APP_NAME=`settings2env APP_NAME settings.yml`
 CI_REGISTRY=`settings2env CI_REGISTRY ${EXT_SETTINGS}`
-CI_REGISTRY_PATH=`settings2env CI_REGISTRY_PATH ${EXT_SETTINGS}`
 
-docker tag $APP_NAME:$TAG  $CI_REGISTRY/$CI_REGISTRY_PATH/$APP_NAME:$CI_COMMIT_TAG
+docker tag $APP_NAME:$TAG  $CI_REGISTRY/$APP_NAME:$CI_COMMIT_TAG
 
 CI_REGISTRY_USER=`settings2env CI_REGISTRY_USER ${EXT_SETTINGS}`
 CI_REGISTRY_PASSWORD=`settings2env CI_REGISTRY_PASSWORD ${EXT_SETTINGS}`
 
-#docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" $CI_REGISTRY
+#docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD"
 
-docker push $CI_REGISTRY/$CI_REGISTRY_PATH/$APP_NAME:$CI_COMMIT_TAG
+docker push $CI_REGISTRY/$APP_NAME:$CI_COMMIT_TAG
 
 PORT_JUPYTERLAB=`settings2env PORT_JUPYTERLAB settings.yml`
 PORT_STREAMLIT=`settings2env PORT_STREAMLIT settings.yml`
@@ -42,7 +41,6 @@ cat base_docker-compose.yml \
     |sed -e "s/PORT_JUPYTERLAB/${PORT_JUPYTERLAB}/g" \
     |sed -e "s/PORT_STREAMLIT/${PORT_STREAMLIT}/g" \
     |sed -e "s/PORT_FREE/${PORT_FREE}/g" \
-    |sed -e "s/CI_REGISTRY_PATH/${CI_REGISTRY_PATH}/g" \
     |sed -e "s/CI_REGISTRY/${CI_REGISTRY}/g" \
     |sed -e "s/APP_NAME/${APP_NAME}/g" \
     |sed -e "s/CI_COMMIT_TAG/${CI_COMMIT_TAG}/g" > docker-compose.yml
