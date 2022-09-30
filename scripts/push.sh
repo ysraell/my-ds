61f7fd504ca88356ae698d1279d06df58ced2548
Israel de Oliveira <israel.de.oliveira@hp.com>
2022-09-30 15:39:38 +0000

    wip

--- scripts/push.sh
+++ scripts/push.sh
@@ -21,28 +21,26 @@ else
 fi
 
 source ./scripts/yaml2env APP_NAME settings.yml
-source ./scripts/yaml2env CI_REGISTRY ${EXT_SETTINGS}
+source ./scripts/yaml2env DOCKER_ACCOUNT settings.yml
 
-docker tag $APP_NAME:$TAG  $CI_REGISTRY/$APP_NAME:$CI_COMMIT_TAG
+docker tag $APP_NAME:$TAG  $DOCKER_ACCOUNT/$APP_NAME:$CI_COMMIT_TAG
 
-source ./scripts/yaml2env CI_REGISTRY_USER ${EXT_SETTINGS}
-source ./scripts/yaml2env CI_REGISTRY_PASSWORD ${EXT_SETTINGS}
+#source ./scripts/yaml2env DOCKER_REGISTRY_USER ${EXT_SETTINGS}
+#source ./scripts/yaml2env DOCKER_REGISTRY_PASSWORD ${EXT_SETTINGS}
+#docker login -u "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD"
 
-#docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD"
+docker push $DOCKER_ACCOUNT/$APP_NAME:$CI_COMMIT_TAG
 
-docker push $CI_REGISTRY/$APP_NAME:$CI_COMMIT_TAG
-
-PORT_JUPYTERLAB=`settings2env PORT_JUPYTERLAB settings.yml`
-PORT_STREAMLIT=`settings2env PORT_STREAMLIT settings.yml`
-PORT_FREE=`settings2env PORT_FREE settings.yml`
+source ./scripts/yaml2env PORT_JUPYTERLAB settings.yml
+source ./scripts/yaml2env PORT_STREAMLIT settings.yml
+source ./scripts/yaml2env PORT_FREE settings.yml
 cat base_docker-compose.yml \
     |sed -e "s/PORT_JUPYTERLAB/${PORT_JUPYTERLAB}/g" \
     |sed -e "s/PORT_STREAMLIT/${PORT_STREAMLIT}/g" \
     |sed -e "s/PORT_FREE/${PORT_FREE}/g" \
-    |sed -e "s/CI_REGISTRY/${CI_REGISTRY}/g" \
+    |sed -e "s/DOCKER_ACCOUNT/${DOCKER_ACCOUNT}/g" \
     |sed -e "s/APP_NAME/${APP_NAME}/g" \
     |sed -e "s/CI_COMMIT_TAG/${CI_COMMIT_TAG}/g" > docker-compose.yml
-
 
 docker-compose -f docker-compose.yml config > docker-stack.yml
 
