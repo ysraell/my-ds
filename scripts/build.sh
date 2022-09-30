4059bfeaed1f232eaad2bd5a258ec1dd22c46061
Israel de Oliveira <israel.de.oliveira@hp.com>
2022-09-30 15:21:24 +0000

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,7 @@ set -e
 source ./scripts/yaml2env APP_NAME settings.yml
 source ./scripts/yaml2env BASE_IMAGE_NAME settings.yml
 source ./scripts/yaml2env BASE_IMAGE_TAG settings.yml
+source ./scripts/yaml2env version settings.yml
+TAG=$version
 
 docker build -t $APP_NAME:$TAG --build-arg TAG=$BASE_IMAGE_TAG --build-arg BASE_IMAGE_NAME=$BASE_IMAGE_NAME --file Dockerfile .
